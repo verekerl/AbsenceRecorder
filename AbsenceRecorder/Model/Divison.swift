8829350d3ae9ba84e73337f513658c71465ee982
Luke Vereker <vereker.l@etoncollege.org.uk>
2023-01-18 15:49:10 +0000

    Adding model for students and divisions

---
+++ AbsenceRecorder/Model/Divison.swift
@@ -0,0 +1,17 @@
+//
+//  Divison.swift
+//  AbsenceRecorder
+//
+//  Created by Luke Vereker on 18/01/2023.
+//
+
+import Foundation
+
+class Division {
+    let code: String
+    var students: [Student] = []
+    
+    init(code: String) {
+        self.code = code
+    }
+}
---
+++ AbsenceRecorder/Model/Student.swift
@@ -0,0 +1,21 @@
+//
+//  Student.swift
+//  AbsenceRecorder
+//
+//  Created by Luke Vereker on 18/01/2023.
+//
+
+import Foundation
+
+class Student {
+    let forename: String
+    let surname: String
+    let birthday: Date
+    
+    
+    init(forename: String, surname: String, birthday: Date) {
+        self.forename = forename
+        self.surname = surname
+        self.birthday = birthday
+    }
+}
